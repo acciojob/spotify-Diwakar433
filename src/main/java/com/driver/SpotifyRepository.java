d9731c5afdd47fd3fa48078a7a2118f11aa66da5
Diwakar Sharma <diwakarchiku12@gmail.com>
2023-08-31 15:15:04 +0530

    th Attempt

--- src/main/java/com/driver/SpotifyRepository.java
+++ src/main/java/com/driver/SpotifyRepository.java
@@ -54,73 +54,78 @@ public class SpotifyRepository {
     }
 
     public Album createAlbum(String title, String artistName) {
-
-        Artist artist = null;
-        for(Artist artist1 : artists) {
-            if(artist.getName() == artistName) {
-                artist = artist1;
+        Artist artist1 = null;
+        for(Artist artist : artists){
+            if(artist.getName()==artistName){
+                artist1 = artist;
                 break;
             }
         }
-
-        if(artist == null) {
-            artist = createArtist(artistName);
+        if(artist1 == null){
+            artist1 = createArtist(artistName);
             Album album = new Album();
             album.setTitle(title);
             album.setReleaseDate(new Date());
+
             albums.add(album);
 
             List<Album> list = new ArrayList<>();
             list.add(album);
-            artistAlbumMap.put(artist,list);
+            artistAlbumMap.put(artist1,list);
             return album;
         }
-        else {
+        else{
             Album album = new Album();
             album.setTitle(title);
             album.setReleaseDate(new Date());
+
             albums.add(album);
 
             List<Album> list = new ArrayList<>();
+            if(list == null){
+                list = new ArrayList<>();
+
+            }
             list.add(album);
-            artistAlbumMap.put(artist, list);
+            artistAlbumMap.put(artist1 ,list);
             return album;
         }
     }
 
     public Song createSong(String title, String albumName, int length) throws Exception{
-
         Album album = null;
-        for(Album album1 : albums) {
-            if(album1.getTitle() == albumName) {
-                album = album1;
+        for(Album album1:albums){
+            if(album1.getTitle()==albumName){
+                album=album1;
                 break;
             }
         }
-
-        if(album == null) {
+        if(album==null)
             throw new Exception("Album does not exist");
-        }
         else {
             Song song = new Song();
             song.setTitle(title);
             song.setLength(length);
             song.setLikes(0);
+
             songs.add(song);
 
-            if(albumSongMap.containsKey(album)) {
-                List<Song> list1 = albumSongMap.get(album);
-                list1.add(song);
-                albumSongMap.put(album, list1);
+//            List<Song> l = albumSongMap.get(album);
+//            l.add(song);
+//            albumSongMap.put(album,l);
+
+            if(albumSongMap.containsKey(album)){
+                List<Song> l = albumSongMap.get(album);
+                l.add(song);
+                albumSongMap.put(album,l);
+            }else{
+                List<Song> songList = new ArrayList<>();
+                songList.add(song);
+                albumSongMap.put(album,songList);
             }
-            else {
-                List<Song> list2 = new ArrayList<>();
-                list2.add(song);
-                albumSongMap.put(album, list2);
-            }
+
             return song;
         }
-
     }
 
     public Playlist createPlaylistOnLength(String mobile, String title, int length) throws Exception {
@@ -339,6 +344,7 @@ public class SpotifyRepository {
         }
     }
 
+
     public String mostPopularArtist() {
         Artist artist = null;
         int max = 0;
@@ -351,8 +357,7 @@ public class SpotifyRepository {
 
         if(artist == null)
             return null;
-        else
-            return artist.getName();
+        return artist.getName();
     }
 
     public String mostPopularSong() {
@@ -368,8 +373,8 @@ public class SpotifyRepository {
 
         if(song == null)
             return null;
-        else
-            return song.getTitle();
+
+        return song.getTitle();
 
     }
 }
